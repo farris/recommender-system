65d534655cae4ba1dcd8371f904853bcc6fc66d8
farreast <fda239@nyu.edu>
2021-04-18 22:47:55 -0700

    “edit”

--- Test.py
+++ Test.py
@@ -25,20 +25,18 @@ def main(spark, file_path):
     
     
     
-    i = 0
+   
     print('------------------------')
-    for f in file_path:
-        if i == 1:
-            break
-        df = spark.read.parquet(str(f))
-        df.createOrReplaceTempView('cf_train')
-        query10 = spark.sql(""" SELECT * from cf_train
-                                LIMIT 10
-                                    """)
-        query10.show()
+   
+    df = spark.read.parquet(str(file_path[0]))
+    df.createOrReplaceTempView('cf_train')
+    query10 = spark.sql(""" SELECT * FROM cf_train
+                            LIMIT 10
+                                """)
+    query10.show()
     
   
-        i+=1
+      
 
 if __name__ == "__main__":
 
