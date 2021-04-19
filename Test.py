6658376bf028231459e3193061a0e425348e942a
farreast <fda239@nyu.edu>
2021-04-18 22:24:30 -0700

    “edit”

--- Test.py
+++ Test.py
@@ -28,8 +28,8 @@ def main(spark, file_path):
         
         print('------------------------')
         df = spark.read.parquet(f)
-        df.createOrReplaceTempView(f)
-        df.show(n=5)
+        df.createOrReplaceTempView('df1')
+        df1.show(n=5)
 
 
 if __name__ == "__main__":
