bafd99b209afa15ea26c784da585352d4f130299
farreast <fda239@nyu.edu>
2021-04-27 14:09:16 -0700

    “edit”

--- Test1.py
+++ Test1.py
@@ -30,12 +30,16 @@ def main(spark, sc):
 
 #%%  Initializing Indexes
  
-    indexers = [StringIndexer(inputCol=column, outputCol=column+"_index").fit(schemaRatings) \
-        for column in list(set(schemaRatings.columns)-set(['count'])-set(['__index_level_0__'])) ]
+    # indexers = [StringIndexer(inputCol=column, outputCol=column+"_index").fit(schemaRatings) \
+    #     for column in list(set(schemaRatings.columns)-set(['count'])-set(['__index_level_0__'])) ]
 
-    pipeline = Pipeline(stages=indexers)
-    indexed = pipeline.fit(schemaRatings).transform(schemaRatings)
-    
+    # pipeline = Pipeline(stages=indexers)
+    # indexed = pipeline.fit(schemaRatings).transform(schemaRatings)
+    path = 'hdfs:/user/fda239/hash'
+    pipelineModel = Pipeline.load(path)
+    indexed = pipelineModel.transform(schemaRatings)
+
+
 #%% Converting Indexes to Int type & selecting necessary columns
 
     indexed.createOrReplaceTempView("ratings_idx")
@@ -45,51 +49,52 @@ def main(spark, sc):
                             
                                            
                             """)
+    results.show()
     
-    results.createOrReplaceTempView("final")
-    cleaned = spark.sql("SELECT userId, trackId ,count FROM final")
+    # results.createOrReplaceTempView("final")
+    # cleaned = spark.sql("SELECT userId, trackId ,count FROM final")
     
 #%% Training the model
 
-    #Test/train split & Fit#######################################
-    (training, test) = cleaned.randomSplit([0.8, 0.2])
-    als = ALS(rank = 10, maxIter=10, regParam=.001,userCol="userId", itemCol="trackId", ratingCol="count",
-                    alpha = .99, implicitPrefs = True,coldStartStrategy="drop")
-    model = als.fit(training)
-    ##############################################################
+    # #Test/train split & Fit#######################################
+    # (training, test) = cleaned.randomSplit([0.8, 0.2])
+    # als = ALS(rank = 10, maxIter=10, regParam=.001,userCol="userId", itemCol="trackId", ratingCol="count",
+    #                 alpha = .99, implicitPrefs = True,coldStartStrategy="drop")
+    # model = als.fit(training)
+    # ##############################################################
 
-    #error########################################################
-    predictions = model.transform(test)
-    evaluator = RegressionEvaluator(metricName="rmse", labelCol="count",
-                                predictionCol="prediction")
-    rmse = evaluator.evaluate(predictions)
-    ##############################################################
+    # #error########################################################
+    # predictions = model.transform(test)
+    # evaluator = RegressionEvaluator(metricName="rmse", labelCol="count",
+    #                             predictionCol="prediction")
+    # rmse = evaluator.evaluate(predictions)
+    # ##############################################################
 
-    print('-----------------------------------------------')
-    print('-----------------------------------------------')
-    print("Root-mean-square error = " + str(rmse))
-    print('-----------------------------------------------')
-    print('-----------------------------------------------')
+    # print('-----------------------------------------------')
+    # print('-----------------------------------------------')
+    # print("Root-mean-square error = " + str(rmse))
+    # print('-----------------------------------------------')
+    # print('-----------------------------------------------')
     
-    print('')
-    print('')
-    print('')
+    # print('')
+    # print('')
+    # print('')
 
-    print('-----------------------------------------------')
-    print('Generate top 10 movie recommendations for a specified set of users')
-    print('-----------------------------------------------')
-    users = cleaned.select(als.getUserCol()).distinct().limit(3)
-    userSubsetRecs = model.recommendForUserSubset(users, 10)
-    userSubsetRecs.show(truncate=False)
-    print('-----------------------------------------------')
-    print('Generate top 10 user recommendations for a specified set of movies')
-    print('-----------------------------------------------') 
-    movies = cleaned.select(als.getItemCol()).distinct().limit(3)
-    movieSubSetRecs = model.recommendForItemSubset(movies, 10)
-    movieSubSetRecs.show(truncate=False)
+    # print('-----------------------------------------------')
+    # print('Generate top 10 movie recommendations for a specified set of users')
+    # print('-----------------------------------------------')
+    # users = cleaned.select(als.getUserCol()).distinct().limit(3)
+    # userSubsetRecs = model.recommendForUserSubset(users, 10)
+    # userSubsetRecs.show(truncate=False)
+    # print('-----------------------------------------------')
+    # print('Generate top 10 user recommendations for a specified set of movies')
+    # print('-----------------------------------------------') 
+    # movies = cleaned.select(als.getItemCol()).distinct().limit(3)
+    # movieSubSetRecs = model.recommendForItemSubset(movies, 10)
+    # movieSubSetRecs.show(truncate=False)
 
 
-    
+
     
 #%% Func call
 if __name__ == "__main__":
