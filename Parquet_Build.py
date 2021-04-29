c6f4dfd846cd27e919b9915e461616e02d62d832
Zafir Momin <zm2114@nyu.edu>
2021-04-29 00:40:45 -0400

    Updating parquet writer

--- Parquet_Build.py
+++ Parquet_Build.py
@@ -9,7 +9,7 @@ from pyspark.ml.recommendation import ALS
 from pyspark.sql import Row
 import sys 
 # %%
-def cleaner(spark,indexed,sc):
+def cleaner(spark,sc,indexed):
     
     indexed.createOrReplaceTempView("ratings_idx")
     results = spark.sql("""
@@ -33,32 +33,33 @@ def main(spark, sc):
     ##############################################
     
     ##### HASH KEY BUILD #####
-    schemaRatings = spark.read.parquet('hdfs:/user/bm106/pub/MSD/cf_train_new.parquet')
-    schemaRatings = schemaRatings.sort(col('__index_level_0__'))
-    print(schemaRatings.rdd.getNumPartitions())
-    indexers = [StringIndexer(inputCol=column, outputCol=column+"_index").fit(schemaRatings) \
-                for column in list(set(schemaRatings.columns)-set(['count'])-set(['__index_level_0__'])) ]
-    pipeline = Pipeline(stages=indexers)
-    indexed = pipeline.fit(schemaRatings)
-    path = 'hdfs:/user/zm2114/hash'
-    indexed.write().overwrite().save(path) 
+#     schemaRatings = spark.read.parquet('hdfs:/user/bm106/pub/MSD/cf_train_new.parquet')
+#     schemaRatings = schemaRatings.sort(col('__index_level_0__'))
+#     print(schemaRatings.rdd.getNumPartitions())
+#     indexers = [StringIndexer(inputCol=column, outputCol=column+"_index").fit(schemaRatings) \
+#                 for column in list(set(schemaRatings.columns)-set(['count'])-set(['__index_level_0__'])) ]
+#     pipeline = Pipeline(stages=indexers)
+#     indexed = pipeline.fit(schemaRatings)
+#     path = 'hdfs:/user/zm2114/hash'
+#     indexed.write().overwrite().save(path) 
     ##### HASH KEY BUILD #####
 
-#     ##### PARQUET BUILD #####
-#     path = 'hdfs:/user/zm2114/hash' 
-#     pipelineModel = PipelineModel.load(path)
+    ##### PARQUET BUILD #####
+    path = 'hdfs:/user/zm2114/hash' 
+    pipelineModel = PipelineModel.load(path)
 
     
-#     #Train########    
-#     df = spark.read.parquet('hdfs:/user/bm106/pub/MSD/cf_'+ files[1] +'.parquet')
-# #     df = df.sort(col('__index_level_0__'))
-#     df = pipelineModel.transform(df) 
-#     final = cleaner(spark,df,sc)
+    #Train########    
+    df = spark.read.parquet('hdfs:/user/bm106/pub/MSD/cf_'+ files[0] +'.parquet')
+    df = df.repartition(1000)
+#     df = df.sort(col('__index_level_0__'))
+    df = pipelineModel.transform(df) 
+    final = cleaner(spark,sc,df)
 
-#     path = 'hdfs:/user/zm2114/cf_'+ files[1] +'.parquet'
+    path = 'hdfs:/user/zm2114/cf_'+ files[0] +'.parquet'
 
-#     final.write.mode("overwrite").parquet(path)
-#     ##### PARQUET BUILD #####
+    final.write.mode("overwrite").parquet(path)
+    ##### PARQUET BUILD #####
 
 #%% Func call
 if __name__ == "__main__":
