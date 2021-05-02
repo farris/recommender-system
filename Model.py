e07f76ff69af165a8de1b7b5c6829393513cf595
farreast <fda239@nyu.edu>
2021-05-02 15:38:26 -0700

    “edit”

--- Model.py
+++ Model.py
@@ -26,7 +26,10 @@ def main(spark, sc):
 
     train = spark.read.parquet(file_path[0])    
     #val = spark.read.parquet(file_path[1]) 
-    #test = spark.read.parquet(file_path[2]) 
+    #test = spark.read.parquet(file_path[2])
+    train.select('user_id',"track_id","count") 
+    print(train.count())
+    print('-----------------------------------------------')
     cols = ['user_id','track_id']
     train = train.drop(*cols)
     train = train.rdd
