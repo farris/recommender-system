fd7318456eab39d6ed2a7808d652f8d1d47b95de
farreast <fda239@nyu.edu>
2021-05-09 17:44:27 -0700

    “edit”

--- GridSearch_All.py
+++ GridSearch_All.py
@@ -40,7 +40,7 @@ def main(spark, sc):
     # -------------------- Running full model. - This ran successfully -------------------------
     # ------------------------------ 10 Recs for each user -------------------------------------
     #            alpha                      regParam                                                    maxIter                   rank
-    params = [ [.0001,.00001,100],                          [1,10,50,75]    ,                                         [5]     ,           [20,70,120,150]        ] 
+    params = [ [.1],                          [1]    ,                                         [2]     ,           [2]        ] 
     params = list(itertools.product(*params))
     #params = params[0:2]
     precision = []
