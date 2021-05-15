9e38f3f96fd5a8ceca25847c98948baa821789ac
Zafir Momin <zm2114@nyu.edu>
2021-05-15 17:29:00 -0400

    Adding new parquet file builder

---
+++ Parquet_Build.py
@@ -0,0 +1,45 @@
+from pyspark.sql import SparkSession
+from pyspark import SparkContext,  SparkConf
+from pyspark.sql.types import *
+from pyspark.sql.functions import col
+from pyspark.ml import Pipeline, PipelineModel
+import sys 
+
+def cleaner(spark,sc,indexed):
+    
+    indexed.createOrReplaceTempView("ratings_idx")
+    results = spark.sql("""
+                            SELECT user_id, track_id, count, CAST(user_id_index AS INT) AS userId , \
+                                CAST(track_id_index AS INT) AS trackId FROM ratings_idx             
+                            """)
+    return results
+
+def main(spark, sc, file_type, repart):
+
+    sc.setLogLevel("OFF")
+    spark.conf.set("spark.blacklist.enabled", "False")
+    spark.conf.set("spark.sql.autoBroadcastJoinThreshold", -1)
+
+    #Read in the written hasher
+    path = 'hdfs:/user/zm2114/hash' 
+    pipelineModel = PipelineModel.load(path)
+    
+    df = spark.read.parquet('hdfs:/user/bm106/pub/MSD/cf_'+ str(file_type) +'.parquet').select('user_id','track_id','count')
+    df = df.repartition(int(repart))
+    df = pipelineModel.transform(df) 
+    final = cleaner(spark,sc,df)
+
+    path = 'hdfs:/user/zm2114/cf_'+ str(file_type) +'.parquet'
+
+    final.write.mode("overwrite").parquet(path)
+
+if __name__ == "__main__":
+
+    # Create the spark session object
+    spark = SparkSession.builder.appName('Parquet_Build').getOrCreate()
+    sc = spark.sparkContext
+    
+    file_type = sys.argv[1]
+    repart = sys.argv[2]
+    
+    main(spark, sc, file_type, repart)
