9a977354c58da27b9bb2811d4b868e8a6a8bcac0
farreast <fda239@nyu.edu>
2021-05-17 11:23:47 -0700

    “edit”

--- GridSearch_All.py
+++ GridSearch_All.py
@@ -14,6 +14,63 @@ def format(df):
     df = df.select('userId',"trackId","count") 
     return df
 
+
+def apk(actual, predicted, k=10):
+    """
+    Computes the average precision at k.
+    This function computes the average prescision at k between two lists of
+    items.
+    Parameters
+    ----------
+    actual : list
+             A list of elements that are to be predicted (order doesn't matter)
+    predicted : list
+                A list of predicted elements (order does matter)
+    k : int, optional
+        The maximum number of predicted elements
+    Returns
+    -------
+    score : double
+            The average precision at k over the input lists
+    """
+    if len(predicted)>k:
+        predicted = predicted[:k]
+
+    score = 0.0
+    num_hits = 0.0
+
+    for i,p in enumerate(predicted):
+        if p in actual and p not in predicted[:i]:
+            num_hits += 1.0
+            score += num_hits / (i+1.0)
+
+    if not actual:
+        return 0.0
+
+    return score / min(len(actual), k)
+
+def mapk(actual, predicted, k=10):
+    """
+    Computes the mean average precision at k.
+    This function computes the mean average prescision at k between two lists
+    of lists of items.
+    Parameters
+    ----------
+    actual : list
+             A list of lists of elements that are to be predicted 
+             (order doesn't matter in the lists)
+    predicted : list
+                A list of lists of predicted elements
+                (order matters in the lists)
+    k : int, optional
+        The maximum number of predicted elements
+    Returns
+    -------
+    score : double
+            The mean average precision at k over the input lists
+    """
+    return np.mean([apk(a,p,k) for a,p in zip(actual, predicted)])
+
 #%% Main
 
 def main(spark, sc):
@@ -54,7 +111,7 @@ def main(spark, sc):
     for i in params:
         
         als = ALS(rank = i[3], maxIter=i[2],regParam=i[1],userCol="userId", itemCol="trackId", ratingCol="count",
-                    alpha = i[0], implicitPrefs = True,coldStartStrategy="drop")
+                    alpha = i[0], implicitPrefs = True)
 
         model = als.fit(train) ##train   
         ##############################################################
@@ -73,7 +130,8 @@ def main(spark, sc):
         print('Join')
         print(k.show(truncate=False))
         print('-----------------------------------------------------')
-
+        
+       
 
         k = k.select('trackId_preds',"trackId").rdd
         print('-----------------------------------------------------')
@@ -84,6 +142,9 @@ def main(spark, sc):
         metrics = RankingMetrics(k)
         
         precision.append(metrics.meanAveragePrecision)
+
+
+        
   
     for i in range(len(params)):
         print(params[i])
